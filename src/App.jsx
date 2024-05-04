957d28ba263ba52141b89ba6dd3bce4dfdf82b72
Pandegar <pandegarauda.webdev@gmail.com>
2024-05-04 22:48:30 +0700

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -282,13 +282,6 @@ function App() {
         a reservation!
       </p>
       <div className="row" data-aos="zoom-in" data-aos-duration="1500">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253840.65639022697!2d106.66470106402271!3d-6.229379586946628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3e945e34b9d%3A0x5371bf0fdad786a2!2sJakarta%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1710926259326!5m2!1sid!2sid"
-          allowFullScreen={true}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          className="map"
-        ></iframe>
 
         <form action="">
           <div className="input-grup">
